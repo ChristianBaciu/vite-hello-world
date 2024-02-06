7a03d76cfcac66c61342193062117c5f092893b3
ChristianBaciu <christian3110@outlook.it>
2024-02-06 19:39:14 +0100

    vite-hello-world

---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    name: "AppHero"
+}
+</script>
+
+
+<template>
+    <img src="@/assets/emoji.PNG" alt="emoji" width="150" height="150"/>
+    <p>Emoji</p>
+</template>
+
+
+<style scoped>
+p{
+    color: rgb(37, 37, 37);
+}
+</style> 
